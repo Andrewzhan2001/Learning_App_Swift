ae92aaac36c91cac6921762a52502d992643fd18
Tianyi Zhan <tianyi.zhan@outlook.com>
2022-04-22 10:13:04 +0800

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by toyika on 2023/2/15.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
